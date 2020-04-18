040a7c5325f17ce2a337c4a91401f286821f6161
bishoybasily <bishoybasily@gmail.com>
2020-04-18 19:55:20 +0200

    modules renaming

--- springframework-commons-jpa/src/main/java/com/github/bishoybasily/springframework/commons/jpa/generator/DatabaseIdGenerator.java
+++ springframework-commons-jpa/src/main/java/com/github/bishoybasily/springframework/commons/jpa/generator/DatabaseIdGenerator.java
@@ -4,6 +4,10 @@ import org.hibernate.engine.spi.SharedSessionContractImplementor;
 
 public class DatabaseIdGenerator extends AbstractIdGenerator {
 
+    public final static String
+            NAME = "DatabaseIdGenerator",
+            CLASS = "com.github.bishoybasily.springframework.commons.jpa.generator.DatabaseIdGenerator";
+
     @Override
     public String generate(SharedSessionContractImplementor session) {
         return session.createNativeQuery("SELECT uuid()").list().iterator().next().toString();
--- springframework-commons-jpa/src/main/java/com/github/bishoybasily/springframework/commons/jpa/generator/JavaIdGenerator.java
+++ springframework-commons-jpa/src/main/java/com/github/bishoybasily/springframework/commons/jpa/generator/JavaIdGenerator.java
@@ -6,6 +6,10 @@ import java.util.UUID;
 
 public class JavaIdGenerator extends AbstractIdGenerator {
 
+    public final static String
+            NAME = "JavaIdGenerator",
+            CLASS = "com.github.bishoybasily.springframework.commons.jpa.generator.JavaIdGenerator";
+
     @Override
     public String generate(SharedSessionContractImplementor session) {
         return UUID.randomUUID().toString();
