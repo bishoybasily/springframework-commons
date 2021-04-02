b825d52a77e434fa4cacfe683607fcd1579c6ba9
bishoybasily <bishoybasily@gmail.com>
2021-04-02 12:57:01 +0200

    handle id getter method while generating the id

--- springframework-commons-jpa/src/main/java/com/github/bishoybasily/springframework/commons/jpa/generator/AbstractIdGenerator.java
+++ springframework-commons-jpa/src/main/java/com/github/bishoybasily/springframework/commons/jpa/generator/AbstractIdGenerator.java
@@ -7,6 +7,8 @@ import org.springframework.util.ObjectUtils;
 
 import javax.persistence.Id;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public abstract class AbstractIdGenerator extends UUIDGenerator {
 
@@ -15,35 +17,76 @@ public abstract class AbstractIdGenerator extends UUIDGenerator {
 
         try {
 
-            Field field = null;
-            Class<?> cls = object.getClass();
-            found:
-            while (!ObjectUtils.isEmpty(cls)) {
-                for (Field f : cls.getDeclaredFields()) {
-                    boolean annotatedWithId = f.isAnnotationPresent(Id.class);
-                    boolean isString = f.getType().getSuperclass().isAssignableFrom(String.class);
-                    if (annotatedWithId && isString) {
-                        field = f;
-                        break found;
-                    }
+          Field field = null;
+          Method method = null;
+
+          Class<?> cls = object.getClass();
+          found:
+          while (!ObjectUtils.isEmpty(cls)) {
+
+            try {
+              for (Method m : cls.getDeclaredMethods()) {
+                boolean annotatedWithId = m.isAnnotationPresent(Id.class);
+                boolean isString = m.getReturnType().getSuperclass().isAssignableFrom(String.class);
+                if (annotatedWithId && isString) {
+                  method = m;
+                  break found;
                 }
-                cls = cls.getSuperclass();
+              }
+            } catch (Exception e) {
+              // ignore this
             }
 
-            if (ObjectUtils.isEmpty(field))
-                throw new RuntimeException();
+            try {
+              for (Field f : cls.getDeclaredFields()) {
+                boolean annotatedWithId = f.isAnnotationPresent(Id.class);
+                boolean isString = f.getType().getSuperclass().isAssignableFrom(String.class);
+                if (annotatedWithId && isString) {
+                  field = f;
+                  break found;
+                }
+              }
+            } catch (Exception e) {
+              // ignore this
+            }
+
+            cls = cls.getSuperclass();
+
+          }
+
+          if (ObjectUtils.isEmpty(method) && ObjectUtils.isEmpty(field))
+            throw new RuntimeException();
+
+          if (!ObjectUtils.isEmpty(field)) {
 
             field.setAccessible(true);
 
             String id = (String) field.get(object);
 
             if (ObjectUtils.isEmpty(id))
-                return generate(session);
+              return generate(session);
 
             return id;
 
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
+          }
+
+          if (!ObjectUtils.isEmpty(method)) {
+
+            method.setAccessible(true);
+
+            String id = (String) method.invoke(object);
+
+            if (ObjectUtils.isEmpty(id))
+              return generate(session);
+
+            return id;
+
+          }
+
+          throw new IllegalStateException();
+
+        } catch (IllegalAccessException | InvocationTargetException e) {
+          throw new RuntimeException(e);
         }
 
     }
