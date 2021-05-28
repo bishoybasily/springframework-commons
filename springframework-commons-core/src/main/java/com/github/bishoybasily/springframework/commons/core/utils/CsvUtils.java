45d4421bd4d98de996cd356ccf302a2fcfb121e2
bishoybasily <bishoybasily@gmail.com>
2021-05-28 21:47:21 +0200

    map flattener

--- springframework-commons-core/src/main/java/com/github/bishoybasily/springframework/commons/core/utils/CsvUtils.java
+++ springframework-commons-core/src/main/java/com/github/bishoybasily/springframework/commons/core/utils/CsvUtils.java
@@ -13,21 +13,21 @@ import java.util.function.Function;
  */
 public class CsvUtils {
 
-	public static Flux<String[]> values(Callable<InputStream> callable) {
-		return FileUtils.lines(callable).map(splitter());
-	}
+    public static Flux<String[]> values(Callable<InputStream> callable, String splitter) {
+        return FileUtils.lines(callable).map(splitter(splitter));
+    }
 
-	public static Flux<String[]> values(InputStream stream) {
-		return FileUtils.lines(stream).map(splitter());
-	}
+    public static Flux<String[]> values(InputStream stream, String splitter) {
+        return FileUtils.lines(stream).map(splitter(splitter));
+    }
 
-	private static Function<String, String[]> splitter() {
-		return s -> {
-			return Arrays.asList(s.split(","))
-					.stream()
-					.map(it -> it.replaceAll("\"", ""))
-					.toArray(String[]::new);
-		};
-	}
+    private static Function<String, String[]> splitter(String splitter) {
+        return s -> {
+            return Arrays.asList(s.split(splitter))
+                    .stream()
+                    .map(it -> it.replaceAll("\"", ""))
+                    .toArray(String[]::new);
+        };
+    }
 
 }
---
+++ springframework-commons-core/src/main/java/com/github/bishoybasily/springframework/commons/core/utils/MapUtils.java
@@ -0,0 +1,24 @@
+package com.github.bishoybasily.springframework.commons.core.utils;
+
+import com.github.bishoybasily.springframework.commons.core.utils.model.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MapUtils {
+
+    public static Map<String, Object> flatten(String root, Map<String, ?> data) {
+        return visit(data, new Node(root), new ArrayList<>()).stream().collect(Collectors.toMap(Node::getPath, Node::getValue));
+    }
+
+    private static List<Node> visit(Map<String, ?> map, Node parent, List<Node> nodes) {
+        map.forEach((k, v) -> {
+            if (v instanceof Map) visit((Map) v, new Node(k).setParent(parent), nodes);
+            else nodes.add(new Node(k).setParent(parent).setValue(v));
+        });
+        return nodes;
+    }
+
+}
